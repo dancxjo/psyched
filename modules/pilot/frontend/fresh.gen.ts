c8a07c5484f982e79feb4cd3262548e41072fca9
Travis Reed <tdreed@gmail.com>
2025-10-01 14:44:23 -0700

    feat(pilot): update route imports in fresh.gen.ts for improved structure

--- modules/pilot/frontend/fresh.gen.ts
+++ modules/pilot/frontend/fresh.gen.ts
@@ -1,12 +1,17 @@
 import config from "./fresh.config.ts";
-import * as $0 from "./routes/index.tsx";
+import * as $0 from "./routes/_app.tsx";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/modules/imu.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest: Manifest = {
     routes: {
-        "./routes/index.tsx": $0,
+        "./routes/_app.tsx": $0,
+        "./routes/index.tsx": $1,
+        "./routes/modules/imu.tsx": $2,
     },
-    islands: {},
+    islands: {
+    },
     baseUrl: import.meta.url,
 };
 
